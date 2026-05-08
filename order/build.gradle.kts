69542b529219938071f4d1d96a4d4107beb83506
K4NEI <kimyoungho0415@gmail.com>
2026-05-08 19:47:27 +0900

    Split modules [order-service order] to ktcloud-msa-order-service

---
+++ order/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm")
+    kotlin("kapt")
+    kotlin("plugin.jpa")
+}
+
+dependencies {
+    implementation("https://maven.pkg.github.com/kanei0415/ktcloud-market-msa-common:1.0.0")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    implementation("org.springframework.kafka:spring-kafka")
+
+    runtimeOnly("com.h2database:h2")
+
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    kapt("jakarta.persistence:jakarta.persistence-api")
+    kapt("jakarta.annotation:jakarta.annotation-api")
+}
+
+sourceSets {
+    main {
+        kotlin.srcDir("build/generated/source/kapt/main")
+    }
+}
